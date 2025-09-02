725e6eed3163d2367c15dfb247d5855409d07b6c
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-09-02 20:44:05 +0200

    Created NavMenu Component

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,24 @@
+const mainMenuItems = [
+  { name: "Home", href: "#home" },
+  { name: "Projects", href: "#projects" },
+  { name: "Get In Touch", href: "#contact" },
+];
+
+export default function NavMenu() {
+  return (
+    <nav>
+      <ul className="flex flex-col gap-y-4">
+        {mainMenuItems.map((item) => (
+          <li key={item.name} className="text-xl tracking-widest">
+            <a
+              href={item.href}
+              className="relative pb-1 text-white after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+            >
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
