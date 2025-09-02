9ceb451066b35d1b5505207819cd9322e34e4c96
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-09-02 18:03:56 +0200

    implemented new bg-component with custom color

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
+import Iridescence from "./Iridescence";
+
+const darkColor = [0.058, 0.125, 0.153];  // #0F2027
+const lightColor = [0.172, 0.325, 0.392]; // #2c5364
+
 function App() {
   return (
     <>
-     <div className="min-h-screen flex flex-col text-white bg-gradient-to-b from-[#0F2027] to-[#2c5364]">
-      
-      <header className="h-[100px] px-8 flex items-center justify-between">
-        <span>Wojciech Potoniec</span>
-        <nav>Menu</nav>
-      </header>
+      <Iridescence
+        className="fixed inset-0 -z-10"
+        color2={darkColor}
+        color1={lightColor}
+        speed={0.5}
+      />
+      <div className="min-h-screen flex flex-col text-white">
+        <header className="h-[100px] px-8 flex items-center justify-between">
+          <span>Wojciech Potoniec</span>
+          <nav>Menu</nav>
+        </header>
 
-      <main className="flex-grow flex justify-center p-8">
-        <div className="w-full max-w-[1200px] text-center">
-          <h1>main</h1>
-          <p>
-            Questo contenuto avrà una larghezza massima di 1200px e sarà
-            centrato.
-          </p>
-        </div>
-      </main>
+        <main className="flex-grow flex justify-center p-8">
+          <div className="w-full max-w-[1200px] text-center">
+            <h1>main</h1>
+            <p>
+              Questo contenuto avrà una larghezza massima di 1200px e sarà
+              centrato.
+            </p>
+          </div>
+        </main>
 
-      <footer className="min-h-[100px] bg-[#333] flex items-center justify-center">
-        <p>© 2025 Il Mio Sito Web</p>
-      </footer>
-    </div>
+        <footer className="min-h-[100px] flex items-center justify-center">
+          <p>© 2025 wojtekhub.it</p>
+        </footer>
+      </div>
     </>
   );
 }
