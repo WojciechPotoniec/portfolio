bf0c6e1de9cfbb615b0b9e5703dff254079cf302
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-09-02 20:43:35 +0200

    created MainContent Component

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+
+export default function MainContent() {
+    return (
+        <main className="flex-grow flex justify-center p-8">
+          <div className="w-full max-w-[1200px] text-center">
+            <h1>main</h1>
+            <p>
+              Questo contenuto avrà una larghezza massima di 1200px e sarà
+              centrato.
+            </p>
+          </div>
+        </main>
+      );
+}
