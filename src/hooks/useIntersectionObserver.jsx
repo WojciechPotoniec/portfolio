ae0f2ad2cef279aa6d5a800a4c7523b42454fd5b
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-09-03 12:20:34 +0200

    Created custom hook for observe if dinamic component are visible in the viewport

---
+++ src/hooks/useIntersectionObserver.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState, useRef } from 'react';
+
+export default function useIntersectionObserver(options) {
+  const [isIntersecting, setIntersecting] = useState(false);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Aggiorna lo stato solo quando l'elemento diventa visibile
+        if (entry.isIntersecting) {
+          setIntersecting(true);
+          observer.unobserve(entry.target); // Smetti di osservare dopo la prima volta
+        }
+      },
+      options
+    );
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    // Funzione di pulizia per disconnettere l'observer
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, [options]);
+
+  return [elementRef, isIntersecting];
+}
