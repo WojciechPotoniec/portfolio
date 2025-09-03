809ace03ce853c8194c0b062fd8293d7fe9bc479
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-09-03 12:23:52 +0200

    Updated a link with a button with a function onClick

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,21 +1,25 @@
 const mainMenuItems = [
-  { name: "Home", href: "#home" },
-  { name: "Projects", href: "#projects" },
-  { name: "Get In Touch", href: "#contact" },
+  { id: "home", name: "About Me", href: "#home" },
+  { id: "projects", name: "Projects", href: "#projects" },
+  { id: "contact", name: "Get In Touch", href: "#contact" },
 ];
 
-export default function NavMenu() {
+export default function NavMenu({ setActiveView, activeView }) {
   return (
     <nav className="p-14 flex">
       <ul className="flex flex-col gap-y-3">
         {mainMenuItems.map((item) => (
           <li key={item.name} className="text-xl tracking-widest">
-            <a
-              href={item.href}
-              className="relative pb-1 text-white after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+            <button
+              onClick={() => setActiveView(item.id)}
+              className={`relative pb-1 after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 ${
+                activeView === item.id
+                  ? "after:w-full"
+                  : "after:w-0 hover:after:w-full"
+              }`}
             >
               {item.name}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
