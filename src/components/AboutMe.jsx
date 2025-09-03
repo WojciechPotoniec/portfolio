9aac54390a16725a1bea810b7e4951d7536483a7
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-09-03 12:18:35 +0200

    Created AboutMe section

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,24 @@
+export default function AboutMe() {
+  return (
+    <div className="text-left animate-fade-in-up">
+      <h1 className="text-4xl font-bold tracking-widest mb-4 uppercase">
+        Su di me
+      </h1>
+      <p className="text-lg leading-relaxed">
+        Ciao ho 30 anni e sono fortemente appasionato di tecnologia in
+        generale, vivo a Benevento nella regione Campania ormai da piú di 20
+        anni, sono di origine polacca e ho conseguito tutti gli studi in Italia.
+        Lego facilmente all'interno di un gruppo e mi piace avere sempre un
+        confronto stimolante, che mi aiuta a trovare soluzioni alternative
+        accrescendo le mie skill di problem solving.
+        <br />
+        <br />
+        Nel tempo libero, mi piace esplorare nuove tecnologie e contribuire
+        a progetti open-source.
+        <br />
+        <br />
+        Quando non scrivo codice, puoi trovarmi a giocare ai VideoGames
+      </p>
+    </div>
+  );
+}
